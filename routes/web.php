d35283e977fe27a6dae81dd0d2cebe01a0f069fe
saori.horiuchi <kinchan3971@gmail.com>
2021-04-07 12:25:45 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,8 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-
+// 認証付きのルーティング(authを使う)
+Route::group(['middleware' => ['auth']], function () {
+    // onlyで作成するルートを絞り込む
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
